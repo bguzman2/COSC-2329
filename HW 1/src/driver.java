b49626ccb51e4d1ef8d6a87a9a76f132158f8a16
Jacob Beneski <jacobbeneski@gmail.com>
2017-09-22 12:40:07 -0500

    HW 1 Complete

--- HW 1/src/driver.java
+++ HW 1/src/driver.java
@@ -20,10 +20,58 @@ public class driver {
         System.out.println("0" + IntegerUtils.isOdd(0));
         System.out.println("-2 " + IntegerUtils.isOdd(-2));
 
+        System.out.println("Checking isPrime()");
+        System.out.println("-3 " + IntegerUtils.isPrime(-3));
+        System.out.println("0 " + IntegerUtils.isPrime(0));
+        System.out.println("1 " + IntegerUtils.isPrime(1));
+        System.out.println("23 " + IntegerUtils.isPrime(23));
+
+
+        System.out.println("Checking isSorted()");
+        int[] testOne = {0};
+        int[] testTwo = {0,1,3,5,7};
+        int[] testThree = {2,6,9,11};
+        int[] testFour = {9,2,5,7,2};
+        int[] testFive = {1,1,2,2,4,4};
+        int[] testSix = {1,3,5,7,9,2};
 
 
 
+        System.out.println("Test One " + IntegerUtils.isSorted(testOne));
+        System.out.println("Test Two " + IntegerUtils.isSorted(testTwo));
+        System.out.println("Test Three " + IntegerUtils.isSorted(testThree));
+        System.out.println("Test Four " + IntegerUtils.isSorted(testFour));
+        System.out.println("Test Five " + IntegerUtils.isSorted(testFive));
+        System.out.println("Test Six " + IntegerUtils.isSorted(testSix));
 
-        //System.out.print(IntegerUtils.getMaximum(intArray));
+
+        System.out.println("Checking getMax() / getMin()");
+        System.out.println("Max: " + IntegerUtils.getMaximum(intArray));
+        System.out.println("Min: " + IntegerUtils.getMinimum(intArray));
+
+
+        System.out.println("Checking getSmallestIndex()");
+        System.out.println("Index: " + IntegerUtils.getSmallestIndexOfMatch(intArray,5));
+        System.out.println("Index: " + IntegerUtils.getSmallestIndexOfMatch(intArray,19));
+
+        System.out.println("Checking greatestCommonFactor()");
+        System.out.println("GCF: " + IntegerUtils.greatestCommonFactor(-2,5));
+        System.out.println("GCF: " + IntegerUtils.greatestCommonFactor(24,32));
+        System.out.println("GCF: " + IntegerUtils.greatestCommonFactor(289,-17));
+        System.out.println("GCF: " + IntegerUtils.greatestCommonFactor(30,42));
+        System.out.println("GCF: " + IntegerUtils.greatestCommonFactor(-21,-21));
+
+        System.out.println("Checking Greatest Constrained Multiple");
+        System.out.println("Max: " + IntegerUtils.getGreatestConstrainedMultiple(7,50));
+        System.out.println("Max: " + IntegerUtils.getGreatestConstrainedMultiple(2,100));
+        System.out.println("Max: " + IntegerUtils.getGreatestConstrainedMultiple(3,19));
+        System.out.println("Max: " + IntegerUtils.getGreatestConstrainedMultiple(10,789));
+        System.out.println("Max: " + IntegerUtils.getGreatestConstrainedMultiple(-2,46));
+        System.out.println("Max: " + IntegerUtils.getGreatestConstrainedMultiple(-52,5));
+
+
+        System.out.println("Checking getIntegerH()");
+        System.out.println("H: " + IntegerUtils.getIntegerH(7,3));
+        System.out.println("H: " + IntegerUtils.getIntegerH(15,0));
     }
 }
--- HW 1/src/utils/IntegerUtils.java
+++ HW 1/src/utils/IntegerUtils.java
@@ -5,43 +5,79 @@ package utils;
  */
 public interface IntegerUtils {
 
-    public static boolean isEven(int k){
+    //Tests to see if the value is even.
+    public static boolean isEven(int k) {
 
         return (k % 2 == 0);
 
     }
 
-    public static boolean isOdd(int k){
+    //Tests to see if the value is odd.
+    public static boolean isOdd(int k) {
 
         return (k % 2 == 1);
     }
 
-    public static boolean isPrime(int k){
-        throw new RuntimeException("NOT IMPLEMENTED");
+    //Tests to see if the value is prime.
+    public static boolean isPrime(int k) {
+
+        //Check if k is a multiple of 2 or less than 1
+        if ((k % 2 == 0) || (k <= 1))
+            return false;
+        //if not, then just check the odds if they are divisible.
+        for (int i = 3; i * i <= k; i += 2) {
+            if (k % i == 0)
+                return false;
+        }
+        return true;
     }
 
-    public static int greatestCommonFactor(int m, int n){
+    //Calculates the greatest common factor of two values.
+    public static int greatestCommonFactor(int m, int n) {
         assert !(m == 0 && n == 0) : "M and N are both Zero!";
 
-        throw new RuntimeException("NOT IMPLEMENTED");
+        //If either variable is 0, return the nonzero value.
+        if ((m == 0) || (n == 0))
+            return m + n;
+        //Else recurse until one value is 0.
+        return Math.abs(greatestCommonFactor(n, m % n));
     }
 
-    public static int getGreatestConstrainedMultiple(int k, int maximum){
-        throw new RuntimeException("NOT IMPLEMENTED");
+    //Calculates the greatest value withing a constrained maximum.
+    public static int getGreatestConstrainedMultiple(int k, int maximum) {
+        return (k * (maximum / k));
     }
 
-    public static int getIntegerH(int h_q_3, int h_r_5){
-        throw new RuntimeException("NOT IMPLEMENTED");
+
+    public static int getIntegerH(int h_q_3, int h_r_5) {
+
+        // H/3 = h_q_3;
+        // H%5 = h_r_5;
+        //Find X
+
+        //Sets an initial value.
+        int firstValue = h_q_3 * 3;
+
+        //Check if the value is divisible by 3 and mod 5.
+        for (int i = firstValue; i < h_q_3 * 4; i++){
+            if (i / 3 == h_q_3 && i % 5 == h_r_5){
+                return i;
+            }
+        }
+        return firstValue;
     }
 
-    public static int getMaximum(int [] intArray) {
+    //Returns the maximum value in an array.
+    public static int getMaximum(int[] intArray) {
 
+        //Sets a temporary max to the first value in the array.
         int tempMax = intArray[0];
 
-        for (int i = 0; i < intArray.length; i++) {
+        //Iterate through the array to see if any value is bigger than tempMax.
+        for (int i = 1; i < intArray.length; i++) {
 
-            if (intArray[i++] >= tempMax) {
-                tempMax = intArray[i++];
+            if (intArray[i] > tempMax) {
+                tempMax = intArray[i];
             }
 
         }
@@ -50,53 +86,54 @@ public interface IntegerUtils {
 
     }
 
-    public static int getMinimum(int[] intArray){
-        throw new RuntimeException("NOT IMPLEMENTED");
+    //Returns the mimimum value in an array.
+    public static int getMinimum(int[] intArray) {
+
+        //Sets a temporary min to the first value in the array.
+        int tempMin = intArray[0];
+
+        //Iterate through the array to see if any value is less than tempMin.
+        for (int i = 1; i < intArray.length; i++) {
+            if (intArray[i] < tempMin) {
+                tempMin = intArray[i];
+            }
+
+        }
+
+        return tempMin;
     }
 
-    public static boolean isSorted(int[] intArray){
+    //Checks to see if an array is sorted.
+    public static boolean isSorted(int[] intArray) {
 
-        boolean sorted = true;
-        for(int i = 0; i < intArray.length; i++){
-            if(intArray[i] <= intArray[i++]){
+        //Iterate through the array to see if any values are out of order.
 
-                //TODO
-                return false;
-
+        for (int i = 0; i < intArray.length - 1; i++) {
+            if (intArray[i] > intArray[i + 1]) {
+                return false; // It is proven that the array is not sorted.
             }
         }
 
-        //TODO
-        return false;
+        //If no values are out of order, we can assume it is in order.
+        return true;
 
     }
 
-    public static int getSmallestIndexOfMatch(int[] intArray, int target){
-        throw new RuntimeException("NOT IMPLEMENTED");
+
+    //Returns to smallest index of a target value.
+    public static int NO_MATCH = -1;
+
+    public static int getSmallestIndexOfMatch(int[] intArray, int target) {
+
+        //Iterate through the array to find the value, and return index.
+        for (int i = 0; i < intArray.length; i++) {
+
+            if (intArray[i] == target)
+                return i;
+        }
+
+        return NO_MATCH;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
